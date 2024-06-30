ef165b4525cd639b3b1bd33898cbbce72c55263b
walid gzouly <wagzouly@gmail.com>
2024-06-30 03:22:39 -0700

    File-Manager

--- utils/db.js
+++ utils/db.js
@@ -12,9 +12,9 @@ class DBClient {
    */
   constructor() {
     envLoader();
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
+    const host = 'localhost';
+    const port = 27017;
+    const database = 'files_manager';
     const dbURL = `mongodb://${host}:${port}/${database}`;
 
     this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
